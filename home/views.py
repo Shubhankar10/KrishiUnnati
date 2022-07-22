c6e33bb87c1510eca04a3f9b21cfdfa5e56fa706
Shreoshi Saharoy <86367528+shreoshiSaharoy@users.noreply.github.com>
2022-07-22 09:18:19 +0530

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,34 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from chatBackend.chat import  chat
-from django.views.generic import TemplateView
 # Create your views here.
-
-class mainpage(TemplateView):
-	Template_view="index.html"
-
-	def get(self,request):
-		return render(request,self.Template_view)
-
-	def post(self,request):
-		if request.method == 'POST':
-			user = request.POST.get('input',False)
-			context={"user":user,"bot":chat(request)}
-			
-		return render(request,self.Template_view,context)
+def home(request):
+    return render(request,'index.html')
 
 def detail(request):
-		return render(request,'details.html')
+    return render(request,'details.html')
 
 def login(request):
-		return render(request,'login.html')
+    return render(request,'login.html')
 
 def signup(request):
-		return render(request,'signup.html')
+    return render(request,'signup.html')
 
 def map(request):
-		return render(request,'map.html')
+    return render(request,'map.html')
 '''
 def detail(request):
-		return render(request,'details.html')
+    return render(request,'details.html')
 '''
