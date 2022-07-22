7e4aec7f74b514d0224f8dc9f637e222d857ab83
Shreoshi Saharoy <86367528+shreoshiSaharoy@users.noreply.github.com>
2022-07-22 09:20:20 +0530

    Update urls.py

--- FARMbit/urls.py
+++ FARMbit/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from home.views import mainpage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    path('',mainpage.as_view(),),
 ]
