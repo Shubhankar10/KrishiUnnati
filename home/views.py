1257bfab27a7e0467db4cc53cc7774dd58554179
Shreoshi Saharoy <86367528+shreoshiSaharoy@users.noreply.github.com>
2022-07-22 09:19:26 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from chatBackend.chat import  chat
+from django.views.generic import TemplateView
 # Create your views here.
-def home(request):
-    return render(request,'index.html')
+
+class mainpage(TemplateView):
+	Template_view="index.html"
+
+	def get(self,request):
+		return render(request,self.Template_view)
+
+	def post(self,request):
+		if request.method == 'POST':
+			user = request.POST.get('input',False)
+			context={"user":user,"bot":chat(request)}
+			
+		return render(request,self.Template_view,context)
 
 def detail(request):
-    return render(request,'details.html')
+		return render(request,'details.html')
 
 def login(request):
-    return render(request,'login.html')
+		return render(request,'login.html')
 
 def signup(request):
-    return render(request,'signup.html')
+		return render(request,'signup.html')
 
 def map(request):
-    return render(request,'map.html')
+		return render(request,'map.html')
 '''
 def detail(request):
-    return render(request,'details.html')
+		return render(request,'details.html')
 '''
