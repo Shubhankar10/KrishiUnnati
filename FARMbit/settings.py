e418c7156902e34df59b53aebe2bd634b489eb16
Shubhankar Tiwary <shubhankartiwary11@gmail.com>
2022-07-30 23:28:48 +0530

    Added Login #1

--- FARMbit/settings.py
+++ FARMbit/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'home',
 ]
 
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'FARMbit.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
@@ -127,3 +128,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+#User Model
+AUTH_USER_MODEL = 'home.NewUser'
+
--- home/urls.py
+++ home/urls.py
@@ -1,16 +1,22 @@
-
-
-
-from django.urls.conf import include
-from . import views
 from .views import mainpage
-from django.urls import include, path
+from django.urls import path,include
+from . import views
+from .views import *
 
 urlpatterns = [
-    path('', mainpage.as_view()),
-    #path('index', views.index, name='index'),
+    # path(' ', mainpage.as_view(), name="index"),
+    path(' ', index, name="index"),
+    path('home', home, name="home"),
+
     path('map', views.map, name='map'),
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
-    path('detail', views.detail, name='detail'),
+    # path('login', views.login, name='login'),
+    # path('detail', views.detail, name='detail'),
+    path("register", register, name="register"),
+    path("login_user", login_user, name="login_user"),
+    path("logout_user", logout_user, name="logout_user"),
 ]
+
+
+'''
+index -> register/login -> home -> logout -> index
+'''
