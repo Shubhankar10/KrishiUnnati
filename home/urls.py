f89c0717d6acdf70bee5ce6bd6afc9923ae05c83
Yash Sakre <72202404+Yash-Sakre@users.noreply.github.com>
2022-08-22 18:38:20 +0530

    Changed urls.py and views.py

--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,4 @@
+
 from .views import mainpage
 from django.urls import path,include
 from . import views
@@ -5,7 +6,7 @@ from .views import *
 
 urlpatterns = [
     # path(' ', mainpage.as_view(), name="index"),
-    path(' ', index, name="index"),
+    path('home', index, name="index"),
     path('home', home, name="home"),
     path('map', views.map, name='map'),
     # path('login', views.login, name='login'),
@@ -14,9 +15,7 @@ urlpatterns = [
     path("login_user", login_user, name="login_user"),
     path("logout_user", logout_user, name="logout_user"),
     path('vr', voice, name='voice'),
+    path("profile1", profile1, name="profile1"),
+    path("profile2", profile2, name="profile2"),
 ]
 
-
-'''
-index -> register/login -> home -> logout -> index
-'''
