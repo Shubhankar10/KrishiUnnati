fe51a8ac9be4de7ca14817dd617c7a7b6a7714ab
Knighthawk-Leo <sanskardwivedi003@gmail.com>
2022-08-26 16:07:39 +0530

    'IMAGEdatabase'

--- imgupload/views.py
+++ imgupload/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from xml.dom.minidom import NamedNodeMap
+from django.shortcuts import render,redirect,HttpResponseRedirect
 from .forms import ImageUploadForm
 from .test01 import *
-
+from django.contrib import messages
+from .connection import *
+import datetime
 
 # Create your views here.
 def imgupload(request):
@@ -33,6 +37,7 @@ def imageprocess(request):
         wiki_url=[]
         dis_desc=[]
         name=[]
+        probability=[]
 
         
 
@@ -40,6 +45,8 @@ def imageprocess(request):
             if(key=='suggestions'):
                 for x1 in value:
                     for y,z in recursive_items(x1):
+
+
                         if(y=='common_names'):
                             #print(z)
                             res_orginal.append(z)
@@ -74,6 +81,8 @@ def imageprocess(request):
             if(key=='diseases'):
                 for x1 in value:
                     for y,z in recursive_items(x1):
+                        if(y=='probability'):
+                            probability.append(z)
                         if(y=='common_names'):
                             #print(z)
                             common_names.append(z)
@@ -91,13 +100,43 @@ def imageprocess(request):
                             a=z[0]
                             similar_images.append(a['url'])
 
+        if(probability[0]<0.6):
+            messages.error(request, 'Our Database not found very good match for this ')
+            return redirect(imgupload)
+            
 
+        t=tables()
+        for x in range(3):
+            con=sql_connection()
+            mycursor = con.cursor()
+            querry="insert into plant values(%s,%s,%s,%s,now())"
+            
+            data=[str(plant_name),str(name[x]),probability[x],'bengluru']
+            mycursor.execute(querry,data)
+            mycursor.close()
+            con.commit()
 
         return render(request,'result.html' , {'res_orginal':res_orginal,'res_value':res_value[0],'img_url':res_value[1],'pclass':pclass,'family':family,'genus':genus,'kindom':kindom,'order':order,'phylum':phylum,'org_url':org_url,'plant_name':plant_name,
         
-        'common_names':common_names,'dis_desc':dis_desc,'wiki_url':wiki_url,'similar_images':similar_images,'name':name})
+        'common_names':common_names,'dis_desc':dis_desc,'wiki_url':wiki_url,'similar_images':similar_images,'name':name,'probability':probability})
 
 def handel_uploaded_file(f):
     with open('img.jpg','wb+')as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+
+def report(request):
+    con=sql_connection()
+    mycursor = con.cursor()
+    querry="select d1,count(d1) from plant where uploaddate BETWEEN (NOW() - INTERVAL 7 DAY) AND NOW() group by d1 order by 2 desc"
+    mycursor.execute(querry)
+    myresult=mycursor.fetchall()
+    name=[]
+    frequency=[]
+    
+    for x in myresult:
+        print(x[0],x[1])
+        name.append(x[0])
+        frequency.append(x[1])
+    zipped=zip(name,frequency)
+    return render(request,"report.html",{'zipped':zipped})
